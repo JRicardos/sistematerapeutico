0d37307b0412e232f906cd67a7abe27d0e9fe791
JRicardos <jricardopas@gmail.com>
2025-08-27 07:07:09 -0300

    json

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,12 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-// src/lib/supabase.js
-console.log('Supabase URL:', process.env.REACT_APP_SUPABASE_URL);
-console.log('Supabase Key:', process.env.REACT_APP_SUPABASE_ANON_KEY ? '✓ Configurada' : '❌ Não encontrada');
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 
 if (!supabaseUrl || !supabaseAnonKey) {
