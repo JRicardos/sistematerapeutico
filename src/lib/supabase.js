fefe9eb3e9d34336a76892cbbf6d863ddd932af4
JRicardos <jricardopas@gmail.com>
2025-09-01 16:55:44 -0300

    TESTE BD

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.REACT_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
+// Verifica se as variáveis de ambiente estão definidas 
 
-if (!supabaseUrl || !supabaseAnonKey) {
+
+if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase URL or key is missing');
 }
 export const supabase = createClient(supabaseUrl, supabaseKey);
